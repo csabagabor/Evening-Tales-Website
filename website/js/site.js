6498c3e5987d78ee58583e0c1f29b12100ce6d36
csaba97 <csabagabor97@google.com>
2018-09-12 20:26:14 +0300

    get title,description from rest api

--- website/js/site.js
+++ website/js/site.js
@@ -1,4 +1,9 @@
 
-$("document").ready(function(){
-  
+$(document).ready(function() {
+    $.ajax({
+        url: "https://lit-wildwood-83335.herokuapp.com/api/tale/"
+    }).then(function(data) {
+       $('#tale-title').append(data.title);
+       $('#tale-description').append(data.description);
+    });
 });
