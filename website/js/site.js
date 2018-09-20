7f91b4ecb49b7400292689035e9cd993b9e8deb0
csaba97 <csabagabor97@google.com>
2018-09-20 19:03:27 +0300

    added URL request param - DATE

--- website/js/site.js
+++ website/js/site.js
@@ -1,9 +1,22 @@
+var apiTaleURL = "https://lit-wildwood-83335.herokuapp.com/api/tale/";
+
+function getTaleByURL(url){
+  let getURL = apiTaleURL;
+  let searchParams = new URLSearchParams(url.search);
+  let paramDate = searchParams.get('date');
+  if(paramDate)
+    getURL+="/"+paramDate;
+  $.ajax({
+      url: getURL
+  }).then(function(data) {
+     $('#tale-title').append(data.title);
+     $('#tale-description').append(data.description);
+  });
+}
 
 $(document).ready(function() {
-    $.ajax({
-        url: "https://lit-wildwood-83335.herokuapp.com/api/tale/"
-    }).then(function(data) {
-       $('#tale-title').append(data.title);
-       $('#tale-description').append(data.description);
-    });
+
+  getTaleByURL(window.location);
+
+
 });
